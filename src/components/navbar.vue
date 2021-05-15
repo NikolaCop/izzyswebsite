f04421ff3e87a93b70fbfc6fd7dbcdf01c8705d6
Nikola Cop <nikolacop05@gmail.com>
2021-05-14 19:29:26 -0600

    init

---
+++ src/components/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { AuthService } from '../services/AuthService'
+import { AppState } from '../AppState'
+import { computed, reactive } from 'vue'
+export default {
+  name: 'Navbar',
+  setup() {
+    const state = reactive({
+      dropOpen: false
+    })
+    return {
+      state,
+      user: computed(() => AppState.user),
+      async login() {
+        AuthService.loginWithPopup()
+      },
+      async logout() {
+        await AuthService.logout({ returnTo: window.location.origin })
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-menu {
+  user-select: none;
+  display: block;
+  transform: scale(0);
+  transition: all 0.15s linear;
+}
+.dropdown-menu.show {
+  transform: scale(1);
+}
+.hoverable {
+  cursor: pointer;
+}
+a:hover {
+  text-decoration: none;
+}
+.nav-link{
+  text-transform: uppercase;
+}
+.nav-item .nav-link.router-link-exact-active{
+  color: var(--primary);
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,45 @@
+<template>
+
+<div class="row">
+<div class="col-12">
+  <h3>RELAX & CONNECT WITH YOUR HOLISTIC BEAUTY</h3>
+</div>
+</div>
+
+<div class="row">
+<div class="col-12">
+PICTURE GOES HERE
+</div>
+</div>
+
+<div class="row">
+<div class="col-12">
+  <h3>
+    YOU’RE IN THE RIGHT PLACE!
+  </h3>
+</div>
+</div>
+
+<div class="row">
+<div class="col-12"></div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+.home{
+  text-align: center;
+  user-select: none;
+  > img{
+    height: 200px;
+    width: 200px;
+  }
+
+}
+</style>
