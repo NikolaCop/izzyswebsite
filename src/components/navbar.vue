8ce87a81dd259f8bec4f269b4232c2e20e582681
Nikola Cop <nikolacop05@gmail.com>
2021-05-21 10:54:57 -0600

    IG Link

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -21,7 +21,7 @@
           <a class="nav-item nav-link text-white pl-5" href="#">Book Online</a>
           <a class="nav-item nav-link text-white pl-5" href="#">New Guest</a>
           <a class="nav-item nav-link text-white pl-5" href="#">Contact</a>
-          <a class="nav-item nav-link text-white pl-5" href="#"><i class="fab fa-instagram"></i></a>
+          <a class="nav-item nav-link text-white pl-5" href="https://www.instagram.com/hairartistizzy/"><i class="fab fa-instagram"></i></a>
         </div>
       </div>
     </nav>
