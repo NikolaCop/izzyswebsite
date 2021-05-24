b5321d32934a7a67708b7c37aabf25ea48efd8be
Nikola Cop <nikolacop05@gmail.com>
2021-05-24 09:21:50 -0600

    navbar change

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -19,7 +19,6 @@
           <a class="nav-item nav-link text-white pl-5" href="#">Services</a>
           <a class="nav-item nav-link text-white pl-5" href="#">Gallery</a>
           <a class="nav-item nav-link text-white pl-5" href="#">Book Online</a>
-          <bookOnlineModal />
           <a class="nav-item nav-link text-white pl-5" href="#">New Guest</a>
           <a class="nav-item nav-link text-white pl-5" href="#">Contact</a>
           <a class="nav-item nav-link text-white pl-5" href="https://www.instagram.com/hairartistizzy/"><i class="fab fa-instagram"></i></a>
