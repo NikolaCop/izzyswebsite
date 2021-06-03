534fb48b37d04b888ca259e472f3b3e890b064f4
Nikola Cop <nikolacop05@gmail.com>
2021-06-03 15:32:34 -0600

    Services Page Start

---
+++ src/pages/ServicesPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ServicesPage">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ServicesPage',
+  props: [],
+  setup() {
+    return {}
+  },
+  components: {}
+}
+</script>
