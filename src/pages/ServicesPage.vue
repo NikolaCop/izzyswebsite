4700cd3b1c3b6cac4f109cafd56b48e4f5a47699
Nikola Cop <nikolacop05@gmail.com>
2021-06-03 16:04:32 -0600

    ServicePage

--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="ServicesPage">
-    SERVICE PAGE YO
+  <div class="container-fluid">
+    <div class="ServicesPage">
+      <div class="row pt-5 pl-5">
+        <div class="col-4">
+          <button type="button" class="btn btn-dark btn-outline-light" id="bookOnlineBTN" data-toggle="modal" data-target="#bookOnlineModal">
+            Book Online!
+          </button>
+        </div>
+        <div class="col-6">
+          <h5>
+            ALL COLOR PRICES ARE STARTING PRICES AND MAY INCREASE IF YOU HAVE PARTICULARLY LONG OR THICK HAIR.
+          </h5>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,3 +27,13 @@ export default {
   components: {}
 }
 </script>
+<style scoped>
+.container-fluid{
+  background-image: url("https://st4.depositphotos.com/5837058/24054/v/600/depositphotos_240548206-stock-illustration-cream-gradient-background-decor.jpg");
+  background-size: cover;
+}
+#bookOnlineBTN{
+  width: 150px;
+  height: 85px;
+}
+</style>
