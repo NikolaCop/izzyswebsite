dfa402852403a693f5f43c35ae93e2ab7ba47d74
Nikola Cop <nikolacop05@gmail.com>
2021-06-04 14:08:50 -0600

    le push

--- src/components/contactModal.vue
+++ src/components/contactModal.vue
@@ -35,7 +35,7 @@
             <div class="row pt-3 pr-5 pl-5">
               <i class="fas fa-undo-alt"></i>
               <p class="tinyWords pl-">
-                Ok, Take Me Back to the Hompage
+                Ok, Take Me Back
               </p>
             </div>
           </div>
