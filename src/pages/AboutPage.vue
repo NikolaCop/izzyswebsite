9416483742c05af5cae1106182c7b73def924b46
Nikola Cop <nikolacop05@gmail.com>
2021-06-08 08:46:26 -0600

    AboutPage

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is the about page</h1>
+    <div class="row p-5">
+      <div class="col-12 text-center">
+        <h3>
+          <i> HI, I’M IZZY! </i>
+        </h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-6 pb-5">
+        <img src="https://images.squarespace-cdn.com/content/v1/600cfbb4f983552f0a570c8e/1614191074272-LV3RWHM0QP57D11QXLZT/ke17ZwdGBToddI8pDm48kH_jyMKD5dc4sWtT72s84KZ7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1URjVRoTo_44URq6EKQD5UVYVCPirsFZhcxCauxpQwDPfxdLwnFHSFwykx4_DJJxhXw/IMG_1865.jpg?format=1500w" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="">
+      </div>
+      <div class="col-6 pr-5 pl-5">
+        <p>
+          As a hair artist, I believe that guests are looking for more than just  hair color! They are looking for a place they can feel comfortable and welcomed by someone who will understand their needs. My ultimate goal is for you to leave my chair feeling more connected to your inner beauty and totally at home in your hair.
+        </p>
+        <br>
+        <p>
+          My passions are keeping your hair healthy and finding styles that suit your lifestyle & maintenance needs. I am so blessed to start my most recent adventure, this February 2021, renting my own chair and running this sweet little business myself!
+        </p>
+        <br>
+        <p>
+          I thrive on creating relationships with my guests and educating you about your hair.  I am committed to using products and practices that are better for the environment, our community and you!
+        </p>
+        <br>
+        <p>
+          For my color and products  I have chosen to work with Aveda, a Vegan and cruelty free line! Using only certified organic ingredients, no synthetic aromas, and ethical ingredient sourcing, these are non negotiable for myself and my guests!
+        </p>
+        <br>
+      </div>
+    </div>
   </div>
 </template>
 
